86ed1f84fb466aa703e9550043b3b7021a70cb2e
wilfredo409 <wilfredo409@yahoo.com>
2023-08-01 01:03:07 -0600

    VLA

--- ProyectoJS/js/crearcurso.js
+++ ProyectoJS/js/crearcurso.js
@@ -1,9 +1,33 @@
 const myModalsuccess = new bootstrap.Modal(document.getElementById('myModalsuccess'));
 
 var formulario = document.getElementById('formulario');
+var apibase = "https://paginas-web-cr.com/ApiPHP/apis/";
+var apicrear = "InsertarCursos.php";
 
 formulario.addEventListener('submit', function(e)
 {
     e.preventDefault();
-    myModalsuccess.show();
+   var datosEnviar = {
+        "nombre" :document.getElementById('nombre').value ,
+        "descripcion" :document.getElementById('descripcion').value ,
+        "tiempo" :document.getElementById('tiempo').value ,
+        "usuario" :"Edwin Ceron"
+
+
+   }
+   apiurl = apibase + apicrear;
+   fetch (apiurl, {
+    method:'POST',
+    body: JSON.stringify(datosEnviar)
+   })
+   .then(estructura => estructura.json())
+   .then((datosrespuesta) => {
+        myModalsuccess.show()
+        completeInsert()
+   })
+   .catch(console.log);
 });
+
+function completeInsert(){
+    window.location = 'listarcurso.html';
+}
--- ProyectoJS/js/listarcurso.js
+++ ProyectoJS/js/listarcurso.js
@@ -1,8 +1,17 @@
+
+
 const newLocal = "https://paginas-web-cr.com/ApiPHP/apis/ListaCurso.php";
 var apiconsultar = newLocal
 var apieliminar = "https://paginas-web-cr.com/ApiPHP/apis/BorrarCursos.php";
+var apibase = "https://paginas-web-cr.com/ApiPHP/apis/";
+var apicrear = "ActualizarCursos.php";
+
 
 const myModalEliminar = new bootstrap.Modal(document.getElementById('myModalEliminar'));
+
+const myModalEditar = new bootstrap.Modal(document.getElementById('myModalEditar'));
+
+const myModalsuccess = new bootstrap.Modal(document.getElementById('myModalsuccess'));
 
 
 let tablaresultado = document.querySelector('#tablaresultado');
@@ -35,7 +44,8 @@ function ajustardatostabla(datos){
            <td>${objetoindividual.tiempo}</td>
            <td>${objetoindividual.usuario}</td>
            <td>
-           <a name="Eliminar" id="Eliminar" class="btn btn-danger" data-bs-toggle="modal" role="button" onclick="mostrarModal('${objetoindividual.id}')">Eliminar</a>
+           <a name="Eliminar" id="Eliminar" class="btn btn-danger"  role="button" data-target="#myModalEliminar" onclick="mostrarModal('${objetoindividual.id}')">Eliminar</a>
+           <a name="Editar" id="editar" class="btn btn-primary"  role="button" data-target="#myModalEditar" onclick="mostrarEditarModal('${objetoindividual.id}','${objetoindividual.nombre}','${objetoindividual.descripcion}','${objetoindividual.tiempo}')">Editar</a>
            </td>
        </tr>`;
         }
@@ -50,6 +60,55 @@ function mostrarModal(id){
    
    
 }
+
+function mostrarEditarModal(id,nombre,descripcion,tiempo){
+    console.log(id,nombre,descripcion,tiempo);
+    document.getElementById('id').value = id;
+    document.getElementById('nombre').value = nombre;
+    document.getElementById('descripcion').value = descripcion;
+    document.getElementById('tiempo').value = tiempo;
+
+    myModalEditar.show();
+   
+    
+    
+
+}
+
+formulario.addEventListener('submit', function(e)
+{
+    
+    e.preventDefault();
+   var datosEnviar = {
+        "id" :document.getElementById('id').value ,
+        "nombre" :document.getElementById('nombre').value ,
+        "descripcion" :document.getElementById('descripcion').value ,
+        "tiempo" :document.getElementById('tiempo').value ,
+        "usuario" :"Edwin Ceron"
+
+
+   }
+   apiurl = apibase + apicrear;
+   fetch (apiurl, {
+    method:'POST',
+    body: JSON.stringify(datosEnviar)
+   })
+   .then(estructura => estructura.json())
+   .then((datosrespuesta) => {
+        myModalsuccess.show()
+        completeInsert()
+   })
+   .catch(console.log);
+});
+
+function completeInsert(){
+    window.location = 'listarcurso.html';
+}
+
+
+
+
+
 
 function eliminandodato(id){
     var datosEnviar = {
