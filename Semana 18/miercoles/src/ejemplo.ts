0139ba9e7c08561238840b62cc8d74eca25e795d
wilfredo409 <wilfredo409@yahoo.com>
2023-10-11 22:02:24 -0600

    VLA

---
+++ Semana 18/miercoles/src/ejemplo.ts
@@ -0,0 +1,12 @@
+
+const titulo = "Semana 18";
+console.log(titulo);
+console.log(typeof titulo);
+
+const numerocualquiera = 10;
+console.log(numerocualquiera);
+console.log(typeof numerocualquiera);
+
+const bolean = false;
+console.log(bolean);
+console.log(typeof bolean);
