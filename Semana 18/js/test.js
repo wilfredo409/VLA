ea5c23fa9f436aa10aaaf7604684c8ef61cebbde
wilfredo409 <wilfredo409@yahoo.com>
2023-10-11 18:35:17 -0600

    VLA

---
+++ Semana 18/js/test.js
@@ -0,0 +1,24 @@
+const titulo = "Semana 18";
+console.log(titulo);
+console.log(typeof titulo);
+
+const numerocualquiera = 10;
+console.log(numerocualquiera);
+console.log(typeof numerocualquiera);
+
+const bolean = false;
+console.log(bolean);
+console.log(typeof bolean);
+
+// Estaticos manejan tipo de datos
+// c++
+//java
+// c#
+
+//por ejemplo 
+//number variable = 0;
+
+// DInamicos, No necesitan saber cual es el tipo de dato
+// js
+// python
+// var variable = 0;
