5921b8ba684e2d243769ccb653ef75967019867f
wilfredo409 <wilfredo409@yahoo.com>
2024-03-04 18:37:40 -0600

    VLA

--- ProyectoReact/ExamenReact/proyecto-react/src/App.tsx
+++ ProyectoReact/ExamenReact/proyecto-react/src/App.tsx
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import TestimonioH from './TestimonioH';
 import banner1 from './img/1.png';
-import banner2 from './img/2.jpg';
-import banner3 from './img/3.png'
 import Menu from './Menu';
 import Home from './Home';
 import { Route, Routes } from 'react-router-dom';
@@ -22,7 +20,7 @@ function App() {
       <Menu></Menu>
       
       <header className="App-header ">
-       <img className="d-block" src={banner1} style={{ width: '70%', height: '500px' }} />
+       <img alt="Imagen predefinida" className="d-block" src={banner1} style={{ width: '70%', height: '500px' }} />
    
      
       
